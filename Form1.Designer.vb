3742783fc77c1c854f4e36428ec5dec1e813aa0d
hemape <hectormp97@gmail.com>
2018-01-15 16:10:07 +0100

    Numero secreto commit

---
+++ Form1.Designer.vb
@@ -0,0 +1,112 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btnGenerarNumero = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.ListView1 = New System.Windows.Forms.ListView()
+        Me.btnLimpiar = New System.Windows.Forms.Button()
+        Me.btnSalir = New System.Windows.Forms.Button()
+        Me.ColorDialog1 = New System.Windows.Forms.ColorDialog()
+        Me.SuspendLayout()
+        '
+        'btnGenerarNumero
+        '
+        Me.btnGenerarNumero.Location = New System.Drawing.Point(62, 29)
+        Me.btnGenerarNumero.Name = "btnGenerarNumero"
+        Me.btnGenerarNumero.Size = New System.Drawing.Size(151, 23)
+        Me.btnGenerarNumero.TabIndex = 0
+        Me.btnGenerarNumero.Text = "Generar nuevo numero"
+        Me.btnGenerarNumero.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(12, 84)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(111, 13)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "Introduce un numero: "
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(147, 81)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(116, 20)
+        Me.TextBox1.TabIndex = 2
+        '
+        'ListView1
+        '
+        Me.ListView1.Location = New System.Drawing.Point(15, 124)
+        Me.ListView1.Name = "ListView1"
+        Me.ListView1.RightToLeft = System.Windows.Forms.RightToLeft.No
+        Me.ListView1.ShowGroups = False
+        Me.ListView1.Size = New System.Drawing.Size(248, 81)
+        Me.ListView1.TabIndex = 3
+        Me.ListView1.UseCompatibleStateImageBehavior = False
+        '
+        'btnLimpiar
+        '
+        Me.btnLimpiar.Location = New System.Drawing.Point(15, 226)
+        Me.btnLimpiar.Name = "btnLimpiar"
+        Me.btnLimpiar.Size = New System.Drawing.Size(75, 23)
+        Me.btnLimpiar.TabIndex = 4
+        Me.btnLimpiar.Text = "Limpiar"
+        Me.btnLimpiar.UseVisualStyleBackColor = True
+        '
+        'btnSalir
+        '
+        Me.btnSalir.Location = New System.Drawing.Point(188, 226)
+        Me.btnSalir.Name = "btnSalir"
+        Me.btnSalir.Size = New System.Drawing.Size(75, 23)
+        Me.btnSalir.TabIndex = 5
+        Me.btnSalir.Text = "Salir"
+        Me.btnSalir.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(284, 261)
+        Me.Controls.Add(Me.btnSalir)
+        Me.Controls.Add(Me.btnLimpiar)
+        Me.Controls.Add(Me.ListView1)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.btnGenerarNumero)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents btnGenerarNumero As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents ListView1 As ListView
+    Friend WithEvents btnLimpiar As Button
+    Friend WithEvents btnSalir As Button
+    Friend WithEvents ColorDialog1 As ColorDialog
+End Class
---
+++ Form1.vb
@@ -0,0 +1,67 @@
+﻿Public Class Form1
+
+    Public numeroGenerado As Integer
+
+
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles btnGenerarNumero.Click
+        'Generar numero del 1 al 100 inlusive'
+        numeroGenerado = CInt(Math.Ceiling(Rnd() * 100)) + 1
+
+        btnGenerarNumero.Enabled = False
+
+
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles btnLimpiar.Click
+
+        numeroGenerado = 0
+
+        btnGenerarNumero.Enabled = True
+
+        ListView1.Clear()
+
+
+
+    End Sub
+
+    'Boton salir'
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles btnSalir.Click
+        Application.Exit()
+
+    End Sub
+
+
+    Private Sub TextBox1_KeyDown(sender As Object, e As KeyEventArgs) Handles TextBox1.KeyDown
+
+        If e.KeyCode = Keys.KeyCode.Enter Then
+
+            'Si el valor es numerico entra'
+            If IsNumeric(TextBox1.Text) Then
+                Dim numeroIntroducido As Integer = CInt(TextBox1.Text)
+                ListView1.Items.Add(numeroIntroducido)
+
+                If numeroIntroducido > numeroGenerado Then
+                    ListView1.Items.Add("El numero secreto es menor")
+
+                ElseIf numeroIntroducido < numeroGenerado Then
+
+                    ListView1.Items.Add("El numero secreto es mayor")
+
+                Else
+                    ListView1.Items.Add("Correcto el numero era " + numeroGenerado.ToString)
+                    numeroGenerado = 0
+                    btnGenerarNumero.Enabled = True
+
+                End If
+
+            Else
+                ListView1.Items.Add("Introduce un numero")
+
+
+            End If
+
+        End If
+    End Sub
+
+End Class
